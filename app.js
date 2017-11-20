0e4d4f222c34250ec088488ea843511112c4263c
Shivangi28071996 <shivangi.gupta0796@gmail.com>
2017-11-20 18:12:11 +0530

    frontend code

--- app.js
+++ app.js
@@ -2,12 +2,12 @@ const express = require('express');
 const http = require('http');
 const path = require('path');
 
-const api = require('./server/routes/api');
+// const api = require('./server/routes/api');
 const app = express();
 
 app.use(express.static(path.join(__dirname,'dist')));
 app.get('*',(req,res)=>{
-    res.sendFile(path.join(__dirname,'/index.html'));
+    res.sendFile(path.join(__dirname,'/dist'));
 });
 
 const port =process.env.PORT || '4200';
